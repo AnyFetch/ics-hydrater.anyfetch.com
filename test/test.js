bba04fa4915430db672a917aaeb5f77775dd1ba0
buathi_q <buathi_q@epitech.eu>
2014-07-04 13:44:21 +0200

    Add ICS hydrater using ics-parser

---
+++ test/test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+require('should');
+
+var Anyfetch = require('anyfetch');
+var anyfetchHydrater = require('anyfetch-hydrater');
+var sinon = require('sinon');
+
+var ics = require('../lib');
+
+var spyPost = sinon.spy(Anyfetch.prototype, 'postDocument');
+var spyDelete = sinon.spy(Anyfetch.prototype, 'deleteDocumentByIdentifier');
+
+var server = Anyfetch.createTestServer();
+server.listen(1338, function() {
+  console.log('Server listen on port 1338');
+});
+
+after(function() {
+  server.close();
+});
+
+describe('Check results of ICS hydrater', function() {
+  it('don\'t have errors', function(done) {
+    var document = {
+      identifier: 'test',
+      access_token: 'test',
+      data: {},
+      metadata: {},
+    };
+
+    var changes = anyfetchHydrater.defaultChanges();
+
+    ics('./test/samples/calendar.ics', document, changes, function(err, changes) {
+      if(err) {
+        done(err);
+      }
+      changes.should.eql({});
+      done();
+    });
+  });
+
+  it('check numbers of document created', function(done) {
+    spyPost.callCount.should.eql(9);
+    done();
+  });
+
+  it('check numbers of document deleted', function(done) {
+    spyDelete.callCount.should.eql(1);
+    done();
+  });
+});
